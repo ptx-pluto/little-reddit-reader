8683b6dab66a640a3bfa9fb219c2d41c82872d87
sheep9275 <sheep9275@gmail.com>
2014-02-05 03:36:40 +0800

    sprite icons added

--- config/compass.rb
+++ config/compass.rb
@@ -1,11 +1,10 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "./css"
-sass_dir = "./sass"
-images_dir = "./sass"
-javascripts_dir = "javascripts"
+
+css_dir = "stylesheets/css"
+sass_dir = "stylesheets/sass"
+images_dir = "stylesheets/sass"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
